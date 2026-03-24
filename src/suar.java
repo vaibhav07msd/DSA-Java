3af7fceb6b6fb9f4fa50b778474a338861f77671
Vaibhav <vaibhavbharti7082@gmail.com>
2026-03-24 14:23:02 +0530

    ye delete krunga...

---
+++ src/suar.java
@@ -0,0 +1,31 @@
+
+
+import java.util.Stack;
+
+public class basic {
+    public static void main(String[] args) {
+         Stack<Integer> st = new Stack<>();
+
+
+         st.push(90);
+        System.out.println(st.size());
+        System.out.println(st);
+         st.push(89);
+        System.out.println(st.size());
+        System.out.println(st);
+         st.push(57);
+        System.out.println(st.size());
+        System.out.println(st);
+         st.push(20);
+        System.out.println(st.size());
+        System.out.println(st);
+         st.push(30);
+        System.out.println(st.size());
+        System.out.println(st);
+         st.push(55);
+
+
+
+    }
+
+}
